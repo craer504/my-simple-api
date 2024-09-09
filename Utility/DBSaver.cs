3c72f96f1aabdce36441f782634735f0938a83eb
samettasti <samet.tasti@dijitalsahne.com>
2024-09-09 16:52:37 +0300

    add db saver class

---
+++ Utility/DBSaver.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleRegisterLoginLogout.Utility
+{
+    public static class DBSaver
+    {
+        private static string? _targetURL = string.Empty;
+
+        public static void SetTargetURL(string targetURL)
+        {
+            _targetURL = targetURL;
+        }
+    }
+}
